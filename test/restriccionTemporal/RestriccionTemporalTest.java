bd7227ca1af874594297d134da4d9705789fbdf5
agustin <agustin@Agustin>
2022-11-05 19:05:16 -0300

    test restriccion

---
+++ test/restriccionTemporal/RestriccionTemporalTest.java
@@ -0,0 +1,19 @@
+package restriccionTemporal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RestriccionTemporalTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
