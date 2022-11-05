778ce00e79e4e7ba322d486bb5345a592f5c717c
agustin <agustin@Agustin>
2022-11-05 02:21:02 -0300

    commit inicial

---
+++ src/muestra/Muestra.java
@@ -0,0 +1,45 @@
+package muestra;
+
+import java.util.Date;
+
+import coordenada.Coordenada;
+import usuario.Usuario;
+
+
+
+public class Muestra {
+	private Coordenada coordenadaGeografica;
+	private Usuario usuario;
+	private Date fechaYHora;
+	
+	
+	
+	public Muestra(Coordenada coordenadaGeografica, Usuario usuario, Date fechaYHora) {
+		super();
+		this.coordenadaGeografica = coordenadaGeografica;
+		this.usuario = usuario;
+		this.fechaYHora = fechaYHora;
+	}
+	
+	
+	public Date getFechaYHora() {
+		return fechaYHora;
+	}
+	
+	
+	@SuppressWarnings("deprecation")
+	public int getSoloHora() {
+		return this.getFechaYHora().getHours();
+	}
+	
+	
+	@SuppressWarnings("deprecation")
+	public int getSoloMinutos() {
+		return this.getFechaYHora().getMinutes();
+	}
+
+	
+	public String getHoraYMinutos() {
+		return this.getSoloHora() + ":" + this.getSoloMinutos();
+	}
+}
