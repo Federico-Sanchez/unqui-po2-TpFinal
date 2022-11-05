3ff52828139ec24f4c76250953e617f86cb35b03
agustin <agustin@Agustin>
2022-11-05 16:48:28 -0300

    cambio en import

---
+++ src/Usuario/Recomendacion.java
@@ -0,0 +1,37 @@
+package usuario;
+
+import java.util.List;
+
+import desafio.Desafio;
+import proyecto.Proyecto;
+
+
+public abstract class  Recomendacion {
+	
+	  public int cantidadDeMuestras;
+	  public int dificultad;
+	  public int recompensa;
+		
+	
+	
+
+	public Recomendacion(int cantidadDeMuestras, int dificultad, int recompensa) {
+		super();
+		this.cantidadDeMuestras = cantidadDeMuestras;
+		this.dificultad = dificultad;
+		this.recompensa = recompensa;
+	}
+
+	public abstract  List<Desafio> seleccionarDesafio(Proyecto proyectos);
+	
+
+	public double coincidenciaDesafio(Desafio desafio,Usuario usuario) {
+		return Math.abs(desafio.cantidadDeMuestrasARecolectar()- usuario.getPerfil().getCantidadDeMuestrasARecolectar()) + 
+			   Math.abs(desafio.dificultad() - this.perfil.getDificultad()) + 
+			   Math.abs(desafio.recompensa() - this.perfil.getRecompensa()); 
+	};
+	
+	
+	
+	
+}
